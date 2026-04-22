670d671e41797fc34ebf0c7b613e2b0659a8aab4
mendojes05 <mendozajessie05@gmail.com>
2026-04-21 22:26:12 -0700

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Orbitron, Space_Grotesk, VT323 } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
@@ -66,6 +67,7 @@ export default function RootLayout({
       <body className={`${orbitron.variable} ${spaceGrotesk.variable} ${vt323.variable} font-sans antialiased`}>
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
         {children}
+        <Analytics />
       </body>
     </html>
   );
